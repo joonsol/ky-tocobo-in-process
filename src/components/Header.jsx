ce5aa2a8d2901ddb93c5de545f9ff50f0bf603ec
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-25 16:28:01 +0900

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Nav from './Nav'
+import Util from './Util'
+import TopBanner from './TopBanner'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <TopBanner />
+
+      <header>
+        <div className="inner">
+          <Nav />
+          <h1 className="tit">TOCOBO</h1>
+          <Util />
+        </div>
+        Header
+      </header>
+    </div>
   )
 }
 
