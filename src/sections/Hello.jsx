0254a9a1d214c94063bbe638f14e42960b704392
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-28 15:53:15 +0900

    .

--- src/sections/Hello.jsx
+++ src/sections/Hello.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Hello = () => {
   return (
-    <div>Hello</div>
+    <div className='inner'>
+        <div className="img-wrap">
+            <img src="" alt="" />
+        </div>
+        <div className="t-wrap">
+            <p className="txt-2"></p>
+            <h2 className="tit"></h2>
+            <p className="txt-1"></p>
+            <a href="" className="btn"></a>
+        </div>
+    </div>
   )
 }
 
