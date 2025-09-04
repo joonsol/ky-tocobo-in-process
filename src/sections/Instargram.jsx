ed5bf1daa58f88e1b805a9bd1084218b1fbf2dd8
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-04 21:07:57 +0900

    .

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Instargram = () => {
   return (
-    <div>Instargram</div>
+    <div className='inner instar-inner'>
+      <div className="t-wrap">
+
+      </div>
+      <ul className="instar-lst">
+        {/* {} */}
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
