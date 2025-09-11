18361d382676248d08c0014cb704cdb4126c593c
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-11 18:47:06 +0900

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,6 +18,9 @@ const Nav = () => {
 
   return (
     <nav>
+      <button className='mob-nav-btn'>
+        <img src="/img/icon_ham.svg" alt="메뉴열기" />
+      </button>
       <ul>
         {navLink.map((item) => (
 
