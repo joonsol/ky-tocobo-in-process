9ce651560ff5c401fc432245e09bba6f1ac9651d
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-19 19:46:41 +0900

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React ,{useState,useEffect,useRef}from 'react';
 import { logoData, companyData, customerCenterData, footerMenus, socialLinks, footerLegal } from "../util/footer";
 
 import "../styles/components/footer.scss"
 
 const Footer = () => {
+
+  const [isOpen, setIsOpen]=useState(false)
+  const hiddenContentRef = useRef(null)
+
+  useEffect(()=>{
+    const handleResize = ()=>{
+      setIsOpen(window.innerWidth>=1111)
+    }
+    handleResize()
+
+    window.addEventListener('resize',handleResize)
+
+    return ()=> window.removeEventListener('resize',handleResize)
+
+  },[])
+
+  useEffect(()=>{
+    const el =hiddenContentRef.current
+
+    if(isOpen){
+      el.style.height =`${el.scrollHeight}px`
+      const onTransitionEnd = ()=>{
+        el.style.height='auto'
+        el.removeEventListener('transitionend',onTransitionEnd)
+      }
+      el.addEventListener('transitionEnd',onTransitionEnd)
+    }else{
+      
+    }
+
+
+
+  },[isOpen])
+
   return (
     <footer className="footer">
       <div className="inner foot-inner">
@@ -51,18 +85,24 @@ const Footer = () => {
         <div className="right">
 
           {/* 고객센터 */}
-          <div>
-            <h4>{customerCenterData.title}</h4>
-            <p className='cs-box'>
-              <a href={customerCenterData.tel.href}>
-                {customerCenterData.tel.value}
+          <div className='cus-wrap '>
+            <h4>
+              {customerCenterData.title}
+              <span className="m-plus"></span>
+            </h4>
+            <div className="hidden" ref={hiddenContentRef}>
+
+              <p className='cs-box'>
+                <a href={customerCenterData.tel.href}>
+                  {customerCenterData.tel.value}
+                </a>
+              </p>
+              <p>{customerCenterData.hours}</p>
+              <p>{customerCenterData.notice}</p>
+              <a className='talk-btn' href={customerCenterData.talk.href}>
+                {customerCenterData.talk.label}
               </a>
-            </p>
-            <p>{customerCenterData.hours}</p>
-            <p>{customerCenterData.notice}</p>
-            <a className='talk-btn' href={customerCenterData.talk.href}>
-              {customerCenterData.talk.label}
-            </a>
+            </div>
           </div>
 
           <div className="footer-legal">
